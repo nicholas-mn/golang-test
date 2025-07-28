55f140a0c4514b0cc38b831c876a481c343d6138
nicholas-mn <9296369+nicholas-mn@users.noreply.github.com>
2025-07-28 20:21:59 +0200

    feat: hello world func

--- hello_test.go
+++ hello_test.go
@@ -3,9 +3,20 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Chris")
-	want := "Hello, Chris"
+	t.Run("saying hello to the people", func(t *testing.T) {
+		got := Hello("Chris")
+		want := "Hello, Chris"
+		assetCorrectMessage(t, got, want)
+	})
+	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+		assetCorrectMessage(t, got, want)
+	})
+}
 
+func assetCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
