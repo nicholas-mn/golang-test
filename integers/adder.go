8d0338084ee034edf48975b9252f87bb8e8e9823
nicholas-mn <9296369+nicholas-mn@users.noreply.github.com>
2025-07-28 20:57:10 +0200

    feat: integers, add function

---
+++ integers/adder.go
@@ -0,0 +1,7 @@
+// Package integers provides utility functions for working with integers.
+package integers
+
+// Add takes two integers and returns the sum of them.
+func Add(x, y int) int {
+	return x + y
+}
