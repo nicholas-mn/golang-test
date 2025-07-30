d3f1f1f5fb20cc5dd8fc8975cc5fd7dc7707dfce
nicholas-mn <9296369+nicholas-mn@users.noreply.github.com>
2025-07-30 16:48:02 +0200

    refactor: table driven test

--- structs_methods_interfaces/shapes_test.go
+++ structs_methods_interfaces/shapes_test.go
@@ -17,21 +17,18 @@ type Shape interface {
 }
 
 func TestArea(t *testing.T) {
-	checkArea := func(t testing.TB, shape Shape, want float64) {
-		t.Helper()
-		got := shape.Area()
-		if got != want {
-			t.Errorf("got %g want %g", got, want)
-		}
+	areaTest := []struct {
+		shape Shape
+		want  float64
+	}{
+		{Rectangle{100.0, 5.0}, 500},
+		{Circle{10}, 314.1592653589793},
 	}
 
-	t.Run("rectangles", func(t *testing.T) {
-		rectangle := Rectangle{100.0, 5.0}
-		checkArea(t, rectangle, 500.0)
-	})
-
-	t.Run("circles", func(t *testing.T) {
-		circle := Circle{10}
-		checkArea(t, circle, 314.1592653589793)
-	})
+	for _, tt := range areaTest {
+		got := tt.shape.Area()
+		if got != tt.want {
+			t.Errorf("got %g want %g", got, tt.want)
+		}
+	}
 }
