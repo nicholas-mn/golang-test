053016a3cde5e041c735e9584a94770928e21309
nicholas-mn <9296369+nicholas-mn@users.noreply.github.com>
2025-07-30 18:19:36 +0200

    refactor: improve test readability

--- pointers_and_errors/wallet.go
+++ pointers_and_errors/wallet.go
@@ -1,7 +1,10 @@
 // Package pointersanderrors ...
 package pointersanderrors
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 type Bitcoin int
 
@@ -17,8 +20,12 @@ func (w *Wallet) Deposit(amount Bitcoin) {
 	w.balance += amount
 }
 
-func (w *Wallet) Withdraw(amount Bitcoin) {
+func (w *Wallet) Withdraw(amount Bitcoin) error {
+	if amount > w.balance {
+		return errors.New("oh no")
+	}
 	w.balance -= amount
+	return nil
 }
 
 func (w *Wallet) Balance() Bitcoin {
