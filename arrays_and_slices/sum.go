1e5166a86bb48f45b034ca5dd60c7c7e1757b6cb
nicholas-mn <9296369+nicholas-mn@users.noreply.github.com>
2025-07-30 14:51:14 +0200

    feat: accept slices with any size

--- arrays_and_slices/sum.go
+++ arrays_and_slices/sum.go
@@ -1,7 +1,7 @@
 // Package arraysandslices learn go with tests
 package arraysandslices
 
-func Sum(numbers [5]int) int {
+func Sum(numbers []int) int {
 	sum := 0
 
 	for _, number := range numbers {
