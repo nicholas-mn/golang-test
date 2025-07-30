e7ad1385ca6bbfd48e025b594419a122ba57903c
nicholas-mn <9296369+nicholas-mn@users.noreply.github.com>
2025-07-30 22:34:06 +0200

    refactor: move assertions out of function for readability

--- pointers_and_errors/wallet_test.go
+++ pointers_and_errors/wallet_test.go
@@ -5,24 +5,6 @@ import (
 )
 
 func TestWallet(t *testing.T) {
-	assertBalance := func(t testing.TB, wallet Wallet, want Bitcoin) {
-		t.Helper()
-		got := wallet.Balance()
-
-		if got != want {
-			t.Errorf("got %s want %s", got, want)
-		}
-	}
-	assertError := func(t testing.TB, err error, want error) {
-		t.Helper()
-		if err == nil {
-			t.Error("wanted an error but didn't get one")
-		}
-		if err != want {
-			t.Errorf("got %q, want %q", err, want)
-		}
-	}
-
 	t.Run("deposit", func(t *testing.T) {
 		wallet := Wallet{}
 		wallet.Deposit(Bitcoin(10))
@@ -43,4 +25,23 @@ func TestWallet(t *testing.T) {
 		assertError(t, err, ErrInsufficientFunds)
 		assertBalance(t, wallet, startingBalance)
 	})
+}
+
+func assertBalance(t testing.TB, wallet Wallet, want Bitcoin) {
+	t.Helper()
+	got := wallet.Balance()
+
+	if got != want {
+		t.Errorf("got %s want %s", got, want)
+	}
+}
+
+func assertError(t testing.TB, err error, want error) {
+	t.Helper()
+	if err == nil {
+		t.Error("wanted an error but didn't get one")
+	}
+	if err != want {
+		t.Errorf("got %q, want %q", err, want)
+	}
 }
