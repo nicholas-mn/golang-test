f6fa552fa6f9ca0726dc23585a494ef320c99ec5
nicholas-mn <9296369+nicholas-mn@users.noreply.github.com>
2025-08-01 13:23:28 +0200

    feat: mocking practice

---
+++ mocking/countdown.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"time"
+)
+
+const (
+	finalWord      = "Go!"
+	countdownStart = 3
+)
+
+func Countdown(out io.Writer) {
+	for i := countdownStart; i > 0; i-- {
+		fmt.Fprintln(out, i)
+		time.Sleep(1 * time.Second)
+	}
+	fmt.Fprint(out, finalWord)
+}
+
+func main() {
+	Countdown(os.Stdout)
+}
---
+++ mocking/go.mod
@@ -0,0 +1,3 @@
+module mocking
+
+go 1.24.5
